4a40e8ebb1657b8b4fc46183c7a9d7d05c7e1053
Neil Seejoor <nvseejoor@gmail.com>
2018-10-07 23:02:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ function BoardMember(name, homeState, training) {
   this.approve = () => {return "You can do that!"}
   this.doCharity = () => {return "I like to help people."}
   this.releasePressStatement = () => {return "You will see great things from Scuber."}
+  this.sayHi = () => {return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
 }
