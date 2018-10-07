5c768bd26959b5fb53530803f6683f8c433cf8fa
Neil Seejoor <nvseejoor@gmail.com>
2018-10-07 22:58:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function BoardMember(name, homeState, training) {
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+  this.veto = function () {
+    return 'No, I must disagree'
+  }
+}
