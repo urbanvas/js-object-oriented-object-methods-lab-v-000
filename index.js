90262aacf80c0c11b79da30947bc611dd93f0a43
Neil Seejoor <nvseejoor@gmail.com>
2018-10-07 23:05:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function BoardMember(name, homeState, training) {
   this.name = name
   this.homeState = homeState
   this.training = training
-  this.veto = () => 'No, I must disagree'
+  this.veto = => 'No, I must disagree'
   this.approve = () => "You can do that!"
   this.doCharity = () => "I like to help people."
   this.releasePressStatement = () => "You will see great things from Scuber."
